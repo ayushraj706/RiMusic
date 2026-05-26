8f81ccaad8abffda22c7e87b1f93482e8550dbba
ayushraj706 <236733393+ayushraj706@users.noreply.github.com>
2026-05-26 14:40:21 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getDatabase } from "firebase/database"; 
+// Agar aap Firestore use karna chahte hain toh 'firebase/firestore' import karein
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -8,3 +12,9 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
+// Initialize Firebase only if it hasn't been initialized already (prevents Next.js hot-reload errors)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const database = getDatabase(app);
+
+export { app, database };
