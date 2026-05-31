69dee0e1c67a81bef87984b45b2efaf4a1978142
ayushraj706 <236733393+ayushraj706@users.noreply.github.com>
2026-05-31 08:28:53 +0530

    Update PropertiesPanel.tsx

--- components/chatbot/PropertiesPanel.tsx
+++ components/chatbot/PropertiesPanel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useEffect, useRef } from "react";
+import { ReactFlow, Background, Controls, addEdge /* baaki cheezein */ } from '@xyflow/react';
 import {
   X,
   Trash2,
